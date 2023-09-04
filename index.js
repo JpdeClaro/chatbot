62a67d5c027503111e904a5c61545764bcf142b3
jpdecalro <jpdeclaro003@gmail.com>
2023-09-05 07:44:43 +0800

    last commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function runBot(){
     // Input your Name
-    let yourName = (prompt("You just clicked me, Im JOYBOY your assistant bot." + "\n What is your name?"));
-        alert("Hi "+yourName);
+    let yourName = (prompt("You just clicked me ^-^, Im JOYBOY your assistant bot." + "\n What is your name?"));
+        alert("Hi "+ yourName + "\n :)");
     // choose anything here!
     let choice=Number(prompt("How can I help you? \n 1. What master do you serve?, 2. Who created you?, 3. Tell me a Joke , 4. Nothing"));
         if (choice == 1 ) {
-            prompt("I serve you Master " + yourName);
+            alert("I serve you Master " + yourName);
 
         }else if (choice == 2){
             alert(" I created by sir Jp DeClaro ");
